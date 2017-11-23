8ce2ccf35de2ed3c0444e8aeca0ed7424a60fbab
unknown <sanchya.bhagat@gmail.com>
2017-11-22 18:18:03 -0800

    fixed get_st path.Added augment() to update flow.

--- NetworkFlowStudy/src/networkflowstudy/tcss543.java
+++ NetworkFlowStudy/src/networkflowstudy/tcss543.java
@@ -12,6 +12,7 @@ import graphCode.Vertex;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.*;
 
 /**
  *
@@ -33,7 +34,7 @@ public class tcss543 {
         c = G.insertVertex(null, "c");
         d = G.insertVertex(null, "d");
         t = G.insertVertex(null, "t");
-        
+        /*
         e1 = G.insertEdge(s, a, 10, "e1");
         e2 = G.insertEdge(b, s, 15, "e2");
         e3 = G.insertEdge(b, a, 15, "e3");
@@ -41,18 +42,25 @@ public class tcss543 {
         e5 = G.insertEdge(d, c, 8, "e5");
         e6 = G.insertEdge(a, d, 6, "e6");
         e7 = G.insertEdge(d, t, 4, "e7");
-        
-        
+        */
+        e1 = G.insertEdge(s, a, 10, "s->a");
+        e2 = G.insertEdge(s, b, 15, "s->b");
+        e3 = G.insertEdge(b, a, 15, "b->a");
+        e4 = G.insertEdge(b, c, 8, "b->c");
+        e5 = G.insertEdge(c, t, 10, "c->t");
+        e6 = G.insertEdge(a, t, 7, "a->t");
+        //e7 = G.insertEdge(d, t, 4, "e7");
+
         Iterator i;
         Vertex v;
         Edge e;
         Vertex[] path = new Vertex[G.numVertices()];
-        
-        int counter = 0;
+
+        //int counter = 0;
         System.out.println("Iterating through vertices...");
         for (i= G.vertices(); i.hasNext(); ) {
             v = (Vertex) i.next();
-            path[counter++] = v;
+            //path[counter++] = v;
             System.out.println("found vertex " + v.getName());
         }
 
@@ -61,38 +69,99 @@ public class tcss543 {
             v = (Vertex) i.next();
             System.out.println("Vertex "+v.getName());
             Iterator j;
-            
+
             for (j = G.incidentEdges(v); j.hasNext();) {
                 e = (Edge) j.next();
                 System.out.println("  found edge " + e.getName());
             }
         }
-        
+
         //Initialize flow
-        HashMap<Edge, Integer> flow = utils.initFlow(G);
+        LinkedHashMap<Edge, Integer> flow = utils.initFlow(G);
         // TODO cleanup
-        for(Entry<Edge,Integer> value : flow.entrySet()) {
-            System.out.println("Edge:"+value.getKey()+" Flow:"+value.getValue());        
-        }
-        
+        //for(Entry<Edge,Integer> value : flow.entrySet()) {
+          //  System.out.println("Edge:"+value.getKey().getName()+" Flow:"+value.getValue());
+        //}
+        System.out.println();
+        System.out.println("Residual graph Before Updating FLOW");
+        System.out.println();
         //Compute Gf
         SimpleGraph Gf = utils.createResidualGraph(G, flow);
-        
+        //int counter = 0;
         System.out.println("Iterating through adjacency lists...");
         for (i= Gf.vertices(); i.hasNext(); ) {
             v = (Vertex) i.next();
-            System.out.println("Vertex "+v.getName());
+            //path[counter++] = v;
+            System.out.println("Vertex "+ v +" "+ v.getName() );
             Iterator j;
-            
+         
             for (j = Gf.incidentEdges(v); j.hasNext();) {
                 e = (Edge) j.next();
-                System.out.println("  found edge " + e.getName());
+               System.out.println("  found edge " + e.getName() + " first= "+ e.getFirstEndpoint().getName()+ "->" + e.getSecondEndpoint().getName() + " Data = " + e.getData());
             }
         }
         
+        // get the current source node 
+        Vertex source = utils.get_s(Gf);
         
-        boolean isSTPath = utils.isSTPath(G, path, s, t);
+        // get the current sink node 
+        Vertex sink = utils.get_t(Gf);
+        //System.out.println("Source: " + source.getName());
+        //System.out.println("Sink: " + sink.getName() );
+
+        // boolean isSTPath = utils.isSTPath(Gf, path, source, sink);
+       // boolean isSTPath = utils.isSTPath(Gf,source, sink);
+        /* print Residual graph Gf
+        for (Iterator j = Gf.edges(); j.hasNext();) {
+                e = (Edge) j.next();
+               System.out.println("  found edge " + e.getName()+" First end = " + e.getFirstEndpoint().getName()
+               + " Second end point:"+e.getSecondEndpoint().getName());
+            }
+           */
+
+        // Create a new LinkedList to store the S-T path
+        List<Vertex> path_new = new LinkedList<>();
         
+        // returns s-t path if it exists, else returns null
+        path_new = utils.getSTPath(Gf, sink, source);
+        if(path_new != null)
+        {
+            path_new.forEach((v11) -> {
+                System.out.println("dir: " + (v11).getName());
+            });
+        }
+        // testing the bottleneck function
+        int bottle_neck = utils.get_bottleneck(Gf, path_new);
+        System.out.println("The bottleneck: " + bottle_neck);
+
+        // test augment() for correctness
+        utils.augment(G, Gf, flow, path_new);
+        
+        //Compute Gf again after updating flow once
+        SimpleGraph Gf2 = utils.createResidualGraph(G, flow);
+        
+        System.out.println("Updated flow now:");
+        for(Map.Entry<Edge,Integer> value : flow.entrySet()) {
+            System.out.println("Edge:"+value.getKey().getName()+" Flow:"+value.getValue());  
+            
+        } 
+        System.out.println();
+        System.out.println("Residual graph After updating the flow ONCE");
+        System.out.println();
+        
+        System.out.println("Iterating through adjacency lists...");
+        for (i= Gf2.vertices(); i.hasNext(); ) {
+            v = (Vertex) i.next();
+            //path[counter++] = v;
+            System.out.println("Vertex "+ v +" "+ v.getName() );
+            Iterator j;
+         
+            for (j = Gf2.incidentEdges(v); j.hasNext();) {
+                e = (Edge) j.next();
+                System.out.println("  found edge " + e.getName()+  " first= "+ e.getFirstEndpoint().getName()+ "->" + e.getSecondEndpoint().getName() + " data =" + e.getData());
+            }
+        }
     }
-    
+
+
 }
--- NetworkFlowStudy/src/networkflowstudy/utils.java
+++ NetworkFlowStudy/src/networkflowstudy/utils.java
@@ -8,7 +8,7 @@ package networkflowstudy;
 import graphCode.Edge;
 import graphCode.SimpleGraph;
 import graphCode.Vertex;
-import java.util.HashMap;
+import java.util.*;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
@@ -20,13 +20,13 @@ import java.util.Stack;
 public class utils {
 
     /**
-     * Initialize the flow Hashmap for the graph G
+     * Initialize the flow LinkedHashmap for the graph G
      *
      * @param G
-     * @return Hashmap containing <Edge, flow value> pairs
+     * @return LinkedHashmap containing <Edge, flow value> pairs
      */
-    public static HashMap<Edge, Integer> initFlow(SimpleGraph G) {
-        HashMap<Edge, Integer> flow = new HashMap<>();
+    public static LinkedHashMap<Edge, Integer> initFlow(SimpleGraph G) {
+        LinkedHashMap<Edge, Integer> flow = new LinkedHashMap<>();
         Iterator i = G.edges();
         while (i.hasNext()) {
             Edge e = (Edge) i.next();
@@ -43,9 +43,10 @@ public class utils {
      * @return Residual Graph
      */
     public static SimpleGraph createResidualGraph(SimpleGraph G,
-            HashMap<Edge, Integer> flow) {
-
-        SimpleGraph Gf = new SimpleGraph();
+            LinkedHashMap<Edge, Integer> flow) {
+        // Initialize an instance of simple graph to be used as Residual Graph
+        SimpleGraph Gf = new SimpleGraph();     
+        // We also maintain a hashmap for the vertices of the new graph Gf for quick access
         HashMap<String, Vertex> vertexList = new HashMap<>();
 
         // Insert all vertex in Gf
@@ -76,11 +77,45 @@ public class utils {
                 Edge be = Gf.insertEdge(vertexList.get(startVertex),
                         vertexList.get(endVertex), flowE, "be");
             }
+            
         }
 
         return Gf;
     }
-    
+    /**
+     * Returns the Source vertex in a given SimpleGraph object
+     * @param Gf
+     * @return source vertex
+     */
+    public static Vertex get_s(SimpleGraph Gf){
+        Iterator i = Gf.vertices();
+        while(i.hasNext()){
+            Vertex v = (Vertex)(i.next());
+            if(v.getName().equals("s")){
+                return v;
+            }
+        }
+        System.out.print("Source was not found!");
+        return null;
+        
+    }
+    /**
+     * Returns the sink vertex in a given SimpleGraph object
+     * @param Gf
+     * @return sink vertex
+     */
+    public static Vertex get_t(SimpleGraph Gf){
+        Iterator i = Gf.vertices();
+        while(i.hasNext()){
+            Vertex v = (Vertex)(i.next());
+            if(v.getName().equals("t")){
+                return v;
+            }
+        }
+        System.out.print("Sink was not found!");
+        return null;
+        
+    }
     
     /**
      * Updates s-t path of graph Gf, if there is a simple path and returns true, 
@@ -91,8 +126,8 @@ public class utils {
      * @param sink
      * @return 
      */
-    public static boolean isSTPath(SimpleGraph Gf, Vertex[] path,
-            Vertex source, Vertex sink) {
+     public static boolean isSTPath(SimpleGraph Gf,
+            Vertex source, Vertex sink){
 
         int numOfVertices = Gf.numVertices();
 
@@ -122,9 +157,9 @@ public class utils {
                 
                 if (!visited[indexOfSecondVertex]) {
                     
-                    path[indexOfSecondVertex] = currVertex;
+                   // path[indexOfSecondVertex] = currVertex;
                     vertexStack.push(secondVertex);
-
+ 
                 }
 
             }
@@ -138,4 +173,173 @@ public class utils {
         }
 
     }
+     /**
+     * Returns s-t path of graph Gf, if there is no simple path , returns 0 
+     *         
+     * @param Gf
+     * @param Source
+     * @param Sink
+     * @return null if no s-t path
+     * else valid s-t path
+     */
+     public static List<Vertex> getSTPath(SimpleGraph Gf, Vertex Sink, Vertex Source){
+         Map<Vertex, Boolean> vis = new HashMap<>();
+         List<Vertex> directions = new LinkedList<>();
+         Queue<Vertex> q = new LinkedList<>();
+         Map<Vertex,Vertex> prev = new HashMap<>();
+         Vertex current = Source;
+         q.add(current);
+         vis.put(current, true);
+         //System.out.println("Sink: "+ Sink.getName() );
+          while(!q.isEmpty()){
+              current = q.poll();
+              //System.out.println("Current "+current.getName());
+              if (current.getName().equals(Sink.getName())){
+                    break;
+              }
+              else{
+                    Iterator i1 = Gf.incidentEdges(current);
+                    while(i1.hasNext()){
+                        Edge e = (Edge)i1.next();
+                        //System.out.println(e_G.getName()+" "+e_G.getFirstEndpoint().getName()+" "+e_G.getSecondEndpoint().getName());
+                        
+                        //TODO check for forward and backward edges
+                        if((e.getFirstEndpoint().getName().equals(current.getName()))){
+                            
+                            Vertex node = e.getSecondEndpoint();
+                             if(!vis.containsKey(node)){
+                                 //System.out.println("node: "+node.getName()); 
+                                q.add(node);
+                                vis.put(node, true);
+                                prev.put(node, current);
+                             }
+                        }
+             
+                    }
+                }
+            }
+            if (!current.getName().equals(Sink.getName())){
+                System.out.println("can't reach destination");
+                return null;
+            }
+            for(Vertex node = Sink; node != null; node = prev.get(node)) {
+                 //System.out.println("dir: " + node.getName());
+                 directions.add(node);
+            }
+      
+            Collections.reverse(directions);
+        
+       /*directions.forEach((v) -> {
+            System.out.println("dir: " + v.getName());
+        }); */
+             return directions;
+          
+    }
+     /**
+     * Returns bottleneck for the given s-t path of graph Gf 
+     *         
+     * @param Gf
+     * @param st_path
+     * @return b_neck
+     */ 
+    public static int get_bottleneck(SimpleGraph Gf, List<Vertex> st_path){
+            ListIterator i = st_path.listIterator();
+            int b_neck = Integer.MAX_VALUE;
+            while(i.hasNext())
+            {
+                Vertex v1 = (Vertex)(i.next());
+                
+                if(i.hasNext())
+                {
+                    Vertex v2 = (Vertex)(i.next());
+                    
+                
+                    Iterator i_edge = Gf.incidentEdges(v1);
+                    while(i_edge.hasNext())
+                    {
+                        Edge e = (Edge)(i_edge.next());
+                        if(Gf.opposite(v1, e).equals(v2))
+                        {
+                            Integer edge_capacity = (Integer)(e.getData());
+                           // System.out.println(v1.getName() + " "+ v2.getName()+ " " + edge_capacity);
+                            if(edge_capacity < b_neck)
+                            {
+                                b_neck = edge_capacity;
+                            }
+                        }
+                    
+                    }
+                    i.previous();
+                } 
+                
+            }
+            return b_neck;
+     }
+     /**
+     * Calculates the increase in flow using get_bottleneck() and updates the flow LinkedHashmap   
+     * @param G
+     * @param Gf        
+     * @param flow
+     * @param path 
+     * 
+     */
+    public static void augment(SimpleGraph G,SimpleGraph Gf,LinkedHashMap<Edge,Integer> flow, List<Vertex> path)
+    {
+        Map<String, Edge> edgeList_G = new HashMap<>();
+        Iterator i1 = G.edges();
+        while (i1.hasNext()) {
+            Edge e_G = (Edge) i1.next();
+            
+            edgeList_G.put(String.valueOf(e_G.getFirstEndpoint().getName()+"->"+e_G.getSecondEndpoint().getName()), e_G);
+            System.out.println("edgelist_G"+ e_G + " Data:" + e_G.getData() + "first and second node"
+                    + e_G.getFirstEndpoint().getName()+" "+ e_G.getSecondEndpoint().getName());
+        }
+        ListIterator i_path = path.listIterator();
+            int b_neck = get_bottleneck(Gf, path);
+            while(i_path.hasNext())
+            {
+                Vertex v1 = (Vertex)(i_path.next());
+                
+                if(i_path.hasNext())
+                {
+                    Vertex v2 = (Vertex)(i_path.next());
+                    
+                
+                    Iterator i_edge = Gf.incidentEdges(v1);
+                    while(i_edge.hasNext())
+                    {
+                        Edge e = (Edge)(i_edge.next());
+                        if(Gf.opposite(v1, e).equals(v2))
+                        {
+                            Integer edge_capacity = (Integer)(e.getData());
+                            // ***TODO Update residual capacity for residual capacity = 0: i_path.e_G. removing edge 
+                            //e.setData(edge_capacity - b_neck);
+                            if(e.getName()=="fe")
+                            {
+                            System.out.println("Edge" + e.getName() + " data now:" + e.getData());
+                            System.out.println(e.getFirstEndpoint().getName()+"->"+e.getSecondEndpoint().getName());
+                            System.out.println((Edge)edgeList_G.get(String.valueOf(e.getFirstEndpoint().getName()+"->"+e.getSecondEndpoint().getName())));
+                            flow.put((Edge)edgeList_G.get(String.valueOf(e.getFirstEndpoint().getName()+"->"+e.getSecondEndpoint().getName())), b_neck);
+                           // System.out.println(v1.getName() + " "+ v2.getName()+ " " + edge_capacity);
+                            }
+                        }
+                    
+                    }
+                    i_path.previous();      // get back to previous element again so that we don't skip a node in teh current s-t path
+                } 
+                
+            }
+            /*
+            for(Map.Entry<Edge,Integer> value : flow.entrySet()) {
+            System.out.println("Edge:"+value.getKey().getName()+" Flow:"+value.getValue());  
+            
+        } */
+    }
 }
+
+
+
+
+
+
+/**/
