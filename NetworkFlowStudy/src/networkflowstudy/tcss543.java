f0d94c31b0ea6f31566e85fdb7c1411390b786d0
Anisha Agarwal <anisha9000@gmail.com>
2017-11-28 22:47:51 -0800

    Integrated preflow push with the rest of the code and code cleanup
    Signed-off-by: Anisha Agarwal <anisha9000@gmail.com>

--- NetworkFlowStudy/src/networkflowstudy/tcss543.java
+++ NetworkFlowStudy/src/networkflowstudy/tcss543.java
@@ -29,117 +29,65 @@ public class tcss543 {
         // TODO code application logic here
         SimpleGraph G = new SimpleGraph();
         GraphInput graphGenrerator = new GraphInput();
-        graphGenrerator.LoadSimpleGraph(G, args[0]);
+        //graphGenrerator.LoadSimpleGraph(G, args[0]);
+        graphGenrerator.LoadSimpleGraph(G, "/home/anisha/AlgoProject/NetworkFlowStudy/src/graphGenerationCode/Mesh/smallMesh.txt");
+        
+        Vertex source = G.getVertex("s");
+        Vertex sink = G.getVertex("t");
         
         long startTime;
         long endTime;
-        
-        /*
-        Vertex s,a,b,c,d,t;
-        Edge e1,e2,e3,e4,e5,e6,e7;
-        s = G.insertVertex(null, "s");
-        a = G.insertVertex(null, "a");
-        b = G.insertVertex(null, "b");
-        c = G.insertVertex(null, "c");
-        d = G.insertVertex(null, "d");
-        t = G.insertVertex(null, "t");
-        
-        e1 = G.insertEdge(s, a, 10, "s->a");
-        e2 = G.insertEdge(s, b, 15, "s->b");
-        e3 = G.insertEdge(b, a, 15, "b->a");
-        e4 = G.insertEdge(b, c, 8, "b->c");
-        e5 = G.insertEdge(c, t, 10, "c->t");
-        e6 = G.insertEdge(a, t, 7, "a->t");
-        //e7 = G.insertEdge(d, t, 4, "e7");
-
-        Iterator i;
-        Vertex v;
-        Edge e;
+        int maxFlow;
+        LinkedHashMap<Edge, Integer> flow;
         
         logging.printGraph(G);
-        /*
-        //Initialize flow
-        LinkedHashMap<Edge, Integer> flow = utils.initFlow(G);
         
-        System.out.println("Residual graph Before Updating FLOW");
-        //Compute Gf
-        SimpleGraph Gf = utils.createResidualGraph(G, flow);
-        
-        utils.printGraph(Gf);
-        
-        // get the current source node 
-        Vertex source = Gf.getVertex("s");
-        
-        // get the current sink node 
-        Vertex sink = Gf.getVertex("t");
-
-        // Create a new LinkedList to store the S-T path
-        //TODO List<Edges>
-        List<Vertex> path_new = new LinkedList<>();
-        
-        // returns s-t path if it exists, else returns null
-        path_new = utils.getSTPath(Gf, sink, source);
-        utils.printPath(path_new);
-        
-        // testing the bottleneck function
-        int bottle_neck = utils.get_bottleneck(Gf, path_new);
-        System.out.println("The bottleneck: " + bottle_neck);
-
-        // test augment() for correctness
-        utils.augment(G, Gf, flow, path_new);
-        
-        //Compute Gf again after updating flow once
-        SimpleGraph Gf2 = utils.createResidualGraph(G, flow);
-        
-        System.out.println("Updated flow now:");
-        for(Map.Entry<Edge,Integer> value : flow.entrySet()) {
-            System.out.println("Edge:"+value.getKey().getName()+" Flow:"+value.getValue());  
-            
-        } 
-        System.out.println();
-        System.out.println("Residual graph After updating the flow ONCE");
-        System.out.println();
-        
-        utils.printGraph(Gf2);
-        */
         /****SCALING MAX FLOW CALCULATION*****/          
-        System.out.println("Calling scaling max flow");
-        logging.printGraph(G);
         
+        System.out.println("Calling scaling max flow");
         startTime = System.currentTimeMillis();
         ScalingMaxFlow scalingMaxFlow = new ScalingMaxFlow(G);
-        Vertex source = G.getVertex("s");
-        Vertex sink = G.getVertex("t");
-        LinkedHashMap<Edge, Integer> flow = scalingMaxFlow.calculateFlow(source, 
+        flow = scalingMaxFlow.calculateFlow(source, 
                 sink);
-        
         //logging.printFlow(flow);
-        
-        int maxFlow = utils.getMaxFlow(G, source, flow);
-        System.out.println("maxFlow:"+ maxFlow);
+        maxFlow = utils.getMaxFlow(G, source, flow);
+        System.out.println("maxFlow from Scaling Max Flow:"+ maxFlow);
         endTime = System.currentTimeMillis();
         SaveOutput.writeToCSV("Mesh","Scaling Max flow", G.numVertices(), 
                 endTime- startTime, maxFlow);
 
+        
+        
+        
         /****FORD FULKERSON MAX FLOW CALCULATION*****/        
         
         System.out.println("Calling Ford fulkerson max flow");
-        logging.printGraph(G);
         
         startTime = System.currentTimeMillis();
         MaxFlow FFmaxFlow = new MaxFlow(G);
-        Vertex source1 = G.getVertex("s");
-        Vertex sink1 = G.getVertex("t");
-        LinkedHashMap<Edge, Integer> flow_FF = FFmaxFlow.calculateFlow(source1, sink1);
-        
+        flow = FFmaxFlow.calculateFlow(source, sink);
         //logging.printFlow(flow);
-        
-        int maxF = utils.getMaxFlow(G, source, flow);
-        System.out.println("maxFlow:"+ maxF);
+        maxFlow = utils.getMaxFlow(G, source, flow);
+        System.out.println("maxFlow:"+ maxFlow);
         endTime = System.currentTimeMillis();
         SaveOutput.writeToCSV("Mesh", "FordFulkerson", G.numVertices(), 
                 endTime- startTime, maxFlow);
         
+        
+        
+        
+        /****FORD FULKERSON MAX FLOW CALCULATION*****/        
+        System.out.println("Calling Preflow Push max flow");
+        
+        startTime = System.currentTimeMillis();
+        PreflowPush preflowPush = new PreflowPush(G);
+        flow = preflowPush.GetMaxFlow();
+        //logging.printFlow(flow);
+        maxFlow = utils.getMaxFlow(G, source, flow);
+        System.out.println("maxFlow:"+ maxFlow);
+        endTime = System.currentTimeMillis();
+        SaveOutput.writeToCSV("Mesh", "PreflowPush", G.numVertices(), 
+                endTime- startTime, maxFlow);
         
     }
 
